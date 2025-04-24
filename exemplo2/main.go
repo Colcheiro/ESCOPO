e84856fbe897b1d0f9bd6fe0eb27a8273d7669ea
Colcheiro <giovanna.b.sousa@aluno.senai.br>
2025-04-24 16:13:01 -0300

    Separado2

---
+++ exemplo2/main.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+var nomeEscola "Escola Técnica SENAI" // variável global
+Coment
+func main() {
+nome: "Lucas" // variável Local
+idade: 16
+// variável Local
+Adicionar d
+mensagem: boasVindas (nome)
+fmt.Println(mensagem)
+status: verificaMaioridade (idade)
+fmt.Println(status)
