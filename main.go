2d0d022c049b946f9d172edb169176c12b724f60
Colcheiro <giovanna.b.sousa@aluno.senai.br>
2025-05-15 08:34:53 -0300

    estruturaCarro

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
  
 import "fmt"
-type Jogador struct{
-	nome string
-	vida int
-	nivel int
-}
-func exibeDados (j Jogador){
-	fmt.Println("O nome do jogador é", j.nome)
-	fmt.Println("A vida do jogador é", j.vida)
-	fmt.Println("O nível do jogador é", j.nivel)
+
+
+type Carro struct{
+	modelo string
+	ano int
+	velocidade int
 }
 
-func main (){
-j1:= Jogador {"Logan",100,3}
-exibeDados(j1)
+func estruturaCarro (c Carro){
+	fmt.Println("O modelo do carro é", c.modelo)
+	fmt.Println("O ano do carro é", c.ano)
+	fmt.Println("A velocidade do carro é", c.velocidade)
+}
+
+func main(){
+	c1:= Carro {"Ford Mustang",2022, 250}
+	estruturaCarro(c1)
 }
